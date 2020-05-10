d570c868d3edc9f7e75b5441df824ee25fac5887
Igor Kornilov <gaber32188@gmail.com>
2020-05-10 14:09:39 +0300

    initialized

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <Container>
+      <div className={styles.Main}></div>
+    </Container>
+  );
+}
+
+export default App;
