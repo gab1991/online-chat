a7afdf54af47f78cae23bf99dc201aec98eb60d6
Igor Kornilov <gaber32188@gmail.com>
2020-08-06 21:53:07 +0300

    CssTransition SignUp Login

--- src/Components/Auth/Login/Backdrop/Backdrop.js
+++ src/Components/Auth/Login/Backdrop/Backdrop.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import DarkPlanet from '../../../UI/SvgBackgroundComponents/DarkPlanet';
 import styles from './Backdrop.module.scss';
 
 export default function Backdrop(props) {
+  const [visible, setVisible] = useState(true);
+
   return (
     <div className={styles.Backdrop}>
-      <CSSTransition
-        timeout={1000}
-        classNames={'list-transition'}
-        onEnter={() => console.log('asdasd')}>
-        <div className={styles.MainPlanetContainer}>
-          <DarkPlanet />
-        </div>
-      </CSSTransition>
+      {/* <CSSTransition
+        timeout={2000}
+        classNames={'MainPlanetContainer-'}
+        in={visible}
+        unmountOnExit
+        appear> */}
+      <div className={styles.MainPlanetContainer}>
+        <DarkPlanet />
+      </div>
+      {/* </CSSTransition> */}
     </div>
   );
 }
