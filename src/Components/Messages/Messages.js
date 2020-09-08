6d17a097c0035a1f8ed9f6a2085c0a253a72453b
Igor Kornilov <gaber32188@gmail.com>
2020-09-08 12:53:52 +0300

    rebuilt on useReducer

--- src/Components/Messages/Messages.js
+++ src/Components/Messages/Messages.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+  useReducer,
+} from 'react';
 import PropTypes from 'prop-types';
 import { debounce, isEmptyObj } from '../../Utils/Utils';
 import Menu from '../Menu/Menu';
@@ -11,8 +17,10 @@ import styles from '../Messages/Messages.module.scss';
 
 function sortConvByTime(convObj) {
   const sortedIds = Object.keys(convObj).sort((chatId1, chatId2) => {
-    const messageArr1 = convObj[chatId1].messages;
-    const messageArr2 = convObj[chatId2].messages;
+    const messageArr1 =
+      convObj[chatId1].matchedMsgs || convObj[chatId1].messages;
+    const messageArr2 =
+      convObj[chatId2].matchedMsgs || convObj[chatId2].messages;
     const lastMsg1Date = Date.parse(
       messageArr1[messageArr1.length - 1].created_at
     );
@@ -37,41 +45,62 @@ function sortConvByTime(convObj) {
   return sortedConvArr;
 }
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FILL_DISPLAYED_CONVS': {
+      const conversations = action.payload;
+      const { matchedConvs, searchInputValue } = state;
+      let displConvs = [];
+
+      if (!isEmptyObj(matchedConvs) || searchInputValue.length !== 0) {
+        displConvs = sortConvByTime(matchedConvs);
+      } else {
+        displConvs = sortConvByTime(conversations);
+      }
+
+      return { ...state, displConvs };
+    }
+    case 'CHANGE_INPUT_VALUE': {
+      const value = action.payload;
+      return { ...state, searchInputValue: value };
+    }
+    case 'SET_IS_SEARCHING': {
+      const value = action.payload;
+      return { ...state, isSearching: value };
+    }
+    case 'SET_MATCHED_CONVS': {
+      const matchedConvs = action.payload;
+      return { ...state, matchedConvs };
+    }
+    default:
+      return state;
+  }
+}
+
 function Messages(props) {
   const { conversations } = props;
   const inputRef = useRef();
-  const [displConvs, setDisplConvs] = useState([]);
+  const [
+    { displConvs, matchedConvs, searchInputValue, isSearching },
+    dispatchLocal,
+  ] = useReducer(reducer, {
+    displConvs: [],
+    matchedConvs: {},
+    isSearching: false,
+    searchInputValue: '',
+  });
   const [showMenu, setShowMenu] = useState(false);
   const [showSearchTab, setSearchTab] = useState(false);
-  const [searchInputValue, setSearchInputValue] = useState('');
-  const [matchedConvs, setMatchedConvs] = useState({});
-  const [isSearching, setIsSearching] = useState(false);
-
-  const displayedConvHandler = () => {
-    let sortedConvArr;
-    if (!isEmptyObj(matchedConvs)) {
-      sortedConvArr = sortConvByTime(matchedConvs);
-    } else if (searchInputValue.length !== 0) {
-      sortedConvArr = [];
-    } else {
-      sortedConvArr = sortConvByTime(conversations);
-    }
-
-    setDisplConvs(sortedConvArr);
-  };
-
-  useEffect(displayedConvHandler, [conversations, matchedConvs]);
-
   const findMessage = (searchStr, conversations) => {
     const foundConvs = {};
-
     if (searchStr.length === 0) {
-      setMatchedConvs(foundConvs);
-      setIsSearching(false);
-
+      dispatchLocal({ type: 'SET_IS_SEARCHING', payload: false });
+      dispatchLocal({
+        type: 'SET_MATCHED_CONVS',
+        payload: foundConvs,
+      });
       return;
     }
-
     for (let id in conversations) {
       const conversation = { ...conversations[id] };
       if (!conversation.messages.length) continue;
@@ -80,12 +109,14 @@ function Messages(props) {
       });
       if (convResult.length) {
         foundConvs[conversation.id] = conversation;
-        foundConvs[conversation.id].matchedMsgs = convResult.reverse();
+        foundConvs[conversation.id].matchedMsgs = convResult;
       }
     }
-    setIsSearching(false);
-
-    setMatchedConvs(foundConvs);
+    dispatchLocal({ type: 'SET_IS_SEARCHING', payload: false });
+    dispatchLocal({
+      type: 'SET_MATCHED_CONVS',
+      payload: foundConvs,
+    });
   };
 
   const delayedSearch = useCallback(debounce(findMessage, 500), [
@@ -94,32 +125,33 @@ function Messages(props) {
   ]);
 
   useEffect(() => {
-    setIsSearching(true);
+    dispatchLocal({ type: 'FILL_DISPLAYED_CONVS', payload: conversations });
+  }, [conversations, matchedConvs]);
 
+  useEffect(() => {
+    if (searchInputValue) {
+      dispatchLocal({ type: 'SET_IS_SEARCHING', payload: true });
+    }
     delayedSearch(searchInputValue, conversations);
-  }, [searchInputValue, delayedSearch, conversations]);
+  }, [searchInputValue, conversations, delayedSearch]);
 
   const enterChat = (conversationID) => {
     props.history.push(`/chats/${conversationID}`);
   };
 
   const toggleSearchInMsgs = () => {
-    setSearchTab((prev) => {
-      if (prev) {
-        setMatchedConvs({});
-        setSearchInputValue('');
-      }
-      return !prev;
-    });
+    setSearchTab((prev) => !prev);
+    dispatchLocal({ type: 'CHANGE_INPUT_VALUE', payload: '' });
+    dispatchLocal({ type: 'SET_MATCHED_CONVS', payload: {} });
   };
 
   const seacrhinputChangeHandler = (e) => {
     const searchStr = e.target.value;
-    setSearchInputValue(searchStr);
+    dispatchLocal({ type: 'CHANGE_INPUT_VALUE', payload: searchStr });
   };
 
   const clearInput = () => {
-    setSearchInputValue('');
+    dispatchLocal({ type: 'CHANGE_INPUT_VALUE', payload: '' });
     inputRef.current.focus();
   };
 
