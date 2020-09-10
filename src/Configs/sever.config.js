dd08fea966f9543fb0a787de25193394962396b8
Igor Kornilov <gaber32188@gmail.com>
2020-09-10 17:42:56 +0300

    production ready fix

--- src/Configs/sever.config.js
+++ src/Configs/sever.config.js
@@ -1,8 +1,4 @@
 const server_adress =
-  process.env.NODE_ENV === 'production'
-    ? 'http://localhost:7000'
-    : 'http://localhost:8000';
-
-console.log(process.env);
+  process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8000';
 
 export { server_adress };
