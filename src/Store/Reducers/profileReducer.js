e4c3ec113e3734bc7beea71f504de529606b094e
Igor Kornilov <gaber32188@gmail.com>
2020-09-14 12:27:42 +0300

    profileReducer cleanUp

--- src/Store/Reducers/profileReducer.js
+++ src/Store/Reducers/profileReducer.js
@@ -1,14 +1,8 @@
-const initial = {
-  avatar_path: localStorage.getItem('avatar_path'),
-};
+const initial = {};
 
 const profileReducer = (state = initial, action) => {
   switch (action.type) {
     case 'UPDATE_PROFILE':
-      const updObj = action.payload;
-      if (updObj.avatarPath && updObj.avatarPath !== state.avatarPath) {
-        localStorage.setItem('avatar_path', updObj.avatarPath);
-      }
       return { ...state, ...action.payload };
     default:
       return state;
