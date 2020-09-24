b52bee151ed1bd0c30b69fdf2f44aac834abb4d2
Igor Kornilov <gaber32188@gmail.com>
2020-09-24 19:03:12 +0300

    Negative lookbehind fix

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -1,13 +1,18 @@
 const regexList = {
   username: /^[a-zA-Z0-9]+$/,
   password: /^(?=.*\d).{4,15}$/,
-  displayed_name: /^(?! ).*(?<! )$/,
+  displayed_name: /^[ \t]+|[ \t]+$/,
   email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
   username_email: /^[a-zA-Z0-9]+$/,
 };
 
 function validate(name, value) {
-  return regexList[name].test(value);
+  //added for browsers do not support negative lookbehind;
+  if (name === 'displayed_name') {
+    return !regexList[name].test(value);
+  } else {
+    return regexList[name].test(value);
+  }
 }
 
 export default validate;
