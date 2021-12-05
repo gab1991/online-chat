77cf82895d063296607c1161df84d028f148591a
Igor.Kornilov <gaber32188@gmail.com>
2021-12-05 13:05:54 +0300

    add gradient button component

---
+++ src/shared/ui/buttons/gradientButton/GradientButton.tsx
@@ -0,0 +1,13 @@
+import { HTMLAttributes } from 'react';
+import cn from 'classnames';
+
+import styles from './GradientButton.module.scss';
+
+interface IPropsGradientButton extends HTMLAttributes<HTMLButtonElement> {
+	light?: boolean;
+}
+
+export function GradientButton(props: IPropsGradientButton) {
+	const { className, light, ...htmlProps } = props;
+	return <button className={cn(styles.gradientBtn, { [styles.gradientBtn_light]: light }, className)} {...htmlProps} />;
+}
